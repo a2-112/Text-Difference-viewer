10a192204d2c6013c2fd27f0e68ec6b1e89ba6e9
a2-112 <juditheziefule1@gmail.com>
2026-09-23 23:08:07 +0100

    Added two more version(percent and local storage)

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const firstCon = document.getElementById("first");
 const secondCon = document.getElementById("second");
-const lcsCon = document.getElementById("lcs-value");
 const compare = document.getElementById("compare");
-const original1 = document.getElementById("original1");
-const original2 = document.getElementById("original2");
-const noMatch = document.getElementById("unmatched");
-const noMatch2 = document.getElementById("unmatched2");
+const history = document.getElementById("all");
+const showHistory = document.getElementById("show");
+const clearHistory = document.getElementById("clear");
+const inputValue = document.getElementById("input-values");
+const lcs_Percent = document.getElementById("match");
+const warning = document.getElementById("warning");
 
 // collect text and makes them reusable
 let value1 = "";
@@ -14,11 +15,36 @@ let value2 = "";
 //Store Sequence
 let sequence = {};
 
+const storage = () => {
+  return JSON.parse(localStorage.getItem("result")) || [];
+};
+const renderHistory = (str1, str2, percent, match, date) => {
+  const output = {
+    id: crypto.randomUUID(),
+    text1: str1,
+    text2: str2,
+    similarity: percent,
+    matchedText: match,
+    date: date.toDateString()
+  };
+  if (
+    !output.text1 ||
+    !output.text2 ||
+    output.similarity === undefined||
+    !output.matchedText ||
+    !output.date
+  )
+    return;
+  const result = storage();
+  result.push(output);
+  localStorage.setItem("result", JSON.stringify(result));
+};
+
 // finding longest common text sequence
 function findLCS(str1, str2, i, j) {
   //base case(when nothing is left)
   if (i === str1.length || j === str2.length) return "";
-//if it exist in the sequence storage
+  //if it exist in the sequence storage
   if (`${i},${j}` in sequence) {
     return sequence[`${i},${j}`];
   }
@@ -43,14 +69,14 @@ try skipping the character from the second string.*/
 // used to find matches from lcs returned storage
 const getMatches = (str, lcs) => {
   const chars = [...str];
-   // Map stores the indexes of characters that matched
+  // Map stores the indexes of characters that matched
   const matches = new Map();
-//index value
+  //index value
   let i = 0;
   let j = 0;
-// Keep going while there are still characters available in BOTH the original text and the LCS.
+  // Keep going while there are still characters available in BOTH the original text and the LCS.
   while (i < chars.length && j < lcs.length) {
-    // if main text and match text are equal 
+    // if main text and match text are equal
     if (chars[i] === lcs[j]) {
       // save the index as a key and value as true
       matches.set(i, true);
@@ -67,30 +93,69 @@ const getMatches = (str, lcs) => {
 };
 // this is based on how the text will be highlighted in the browser
 const highlight = (str, matches, i) => {
-// Convert the string into Unicode-aware characters
-// so characters like emoji are not split by str[i]
+  // Convert the string into Unicode-aware characters
+  // so characters like emoji are not split by str[i]
   const chars = [...str];
-  // var that holds the html display result 
+  // var that holds the html display result
   let output = "";
   //base case
   if (i === chars.length) return "";
-// if the index exist in matches object
+  // if the index exist in matches object
   if (matches.has(i)) {
-    //create a span of match for it 
+    //create a span of match for it
     output += `<span class="match">${chars[i]}</span>`;
   } else {
-    // create a span of different 
+    // create a span of different
     output += `<span class="difference">${chars[i]}</span>`;
   }
-// return the format and rerun again
+  // return the format and rerun again
   return output + highlight(str, matches, i + 1);
 };
-// mainly for displaying result in html
-const display = (str1, str2, result) => {
-  original1.innerHTML = `<span class ="main-text">Original Text 1:</span> ${str1}`;
-  original2.innerHTML = `<span class ="main-text">Original Text 2:</span> ${str2}`;
-  lcsCon.innerHTML = `<span id="common">Matched Text:</span> ${result}`;
+
+const percentage = (str1, str2, lcs) => {
+  const longest = Math.max(str1.length, str2.length);
+  const percent = Math.floor((lcs.length / longest) * 100);
+  return percent;
 };
+
+// mainly for displaying result in html
+const display = (str1, str2, result, percent) => {
+  inputValue.innerHTML = `
+  <p><span>Original Text 1: </span>${str1}</p> 
+  <p><span>Original Text 2: </span>${str2}</p>`;
+  lcs_Percent.innerHTML = `
+  <p><span>Matched Text: </span>${result}</p> 
+  <p>${percent}%</p>`;
+};
+
+const displayHistory = () => {
+  const items = storage();
+  const all = items.map((item) => {
+    return `
+  <div class="card" data-id="${item.id}">
+  <div class ="content">
+  <ul>
+  <li><span>Text 1: </span>${item.text1}</li>
+  <li><span>Text 2: </span>${item.text2}</li>
+  <li><span>Similarity: </span>${item.similarity}%</li>
+  <li><span>Matched Text: </span>${item.matchedText}</li>
+  <li><span>Date: </span>${item.date}</li>
+  </ul>
+  </div>
+  <div class ="card-btn">
+  <button class="delete">Delete</button>
+  </div>
+  </div>
+  `;
+  }).join("");
+  history.innerHTML = all
+}
+
+const deleteFunction = (id) => {
+  const memory = storage()
+  const remove = memory.filter(item => item.id !== id)
+  localStorage.setItem("result",JSON.stringify(remove))
+}
 
 // At new input clear Sequence
 const clear = () => {
@@ -103,11 +168,42 @@ firstCon.addEventListener("input", () => {
 // at typing take it text
 secondCon.addEventListener("input", () => {
   value2 = secondCon.value.toLowerCase();
-});
+}); 
+
+history.addEventListener("click", (e) => {
+  if(e.target.closest(".delete")){
+  const card = e.target.closest(".card").dataset.id
+  deleteFunction(card)
+    displayHistory()
+  }
+})
+
+showHistory.addEventListener("click", () => {
+   const isHidden= history.toggleAttribute("hidden");
+  if(!isHidden){
+    showHistory.innerText = "Close History"
+      displayHistory()
+  }else {
+    showHistory.innerText = "Show History";
+  }
+})
+
+clearHistory.addEventListener("click", () => {
+  history.innerHTML = ""
+  history.hidden = true
+  showHistory.innerText = "Show History";
+  localStorage.clear()
+})
+
 // Compare both texts and display the LCS result
 compare.addEventListener("click", () => {
+if (!value1 || !value2) {
+  warning.innerText = "Please enter text in both fields.";
+  return;
+}
+warning.innerText = "";
 // Convert the string into Unicode-aware characters
-// so characters like emoji are not split by str[i]
+  // so characters like emoji are not split by str[i]
   const chars1 = [...value1];
   const chars2 = [...value2];
   const result = findLCS(chars1, chars2, 0, 0);
@@ -115,8 +211,11 @@ compare.addEventListener("click", () => {
   const matches2 = getMatches(value2, result);
   const main = highlight(value1, matches, 0);
   const main2 = highlight(value2, matches2, 0);
-
-  display(main, main2, result);
+  const percent = percentage(chars1, chars2, result);
+  const date = new Date();
+  renderHistory(value1, value2, percent, result, date);
+  display(main, main2, result, percent);
 });
 // in the body once there is a new type reset sequence storage
 document.addEventListener("input", clear);
+displayHistory()
